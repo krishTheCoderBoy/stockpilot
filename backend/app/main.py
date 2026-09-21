ead72ca6656be999972de1af0d54f8827f547fe9
krishTheCoderBoy <kd743452@gmail.com>
2026-09-21 16:56:34 +0530

    changes in backend not reflected

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,7 +30,7 @@ app.include_router(dashboard_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","http://localhost:5174/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
